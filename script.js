69d054072315f6d1cf437496c9fde9b8bba0c853
Samuel Ramos <samuelvanguarda@gmail.com>
2024-06-20 19:06:26 -0300

    FEAT: adds the event turnOff to a button

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const turnOnBtn = document.getElementById('turnOn');
 const turnOffBtn = document.getElementById('turnOff');
 const lampImg = document.getElementById('lamp');
 
+turnOffBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  lampImg.src != './img/quebrada.jpg' && (lampImg.src = './img.desligada.jpg');
+})
